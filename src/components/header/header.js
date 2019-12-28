f8d34fa61193d3bdb4a562b1b1e71b49a45810dd
sarru1291 <sarru1291@gmail.com>
2019-12-28 23:37:52 +0530

    header added

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.scss';
+
+export default function Header(props) {
+    return (
+        <div className="header">
+            <h1><span>the</span>Volume</h1>
+        </div>
+    )
+}
